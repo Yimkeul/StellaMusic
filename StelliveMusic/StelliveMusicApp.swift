fa7f2dbd37aadd004c025ee7f86b3f7cc1b3f6e1
Yimkeul <leesjun29@gmail.com>
2024-09-04 01:14:43 +0900

    upload

---
+++ StelliveMusic/StelliveMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  StelliveMusicApp.swift
+//  StelliveMusic
+//
+//  Created by yimkeul on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StelliveMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
